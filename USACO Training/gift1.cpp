7cca7006e09b17de978df31e9800f7c63ae199a1
pranavb1234 <pranavtech@rediffmail.com>
2024-12-11 19:17:16 +0530

    first1

--- USACO Training/gift1.cpp
+++ USACO Training/gift1.cpp
@@ -12,29 +12,7 @@ LANG: C++
 
 using namespace std;
 
-int getD(int Y, int M){
-    if (M==1 || M==3 || M==5 || M==7 || M==8 || M==10 || M==12)
-        {
-            return 31;
-        }
-        else if (M==4 || M==6 || M==9 || M==11)
-        {
-            return 30;
-        }
-        else
-        {
-            if ((Y%100==0 && Y%400==0) || (Y%100!=0 && Y%4==0))
-            {
-                return 29;
-            }
-            else
-            {
-                return 28;
-            }
-        }
-            
-    
-}
+
 
 int main() {
     string input_file = "gift1.in";
